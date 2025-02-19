b90a271b23b62083af1eeb5efe7b67f2a155ba4c
Jerresand <gustavjerresand@gmail.com>
2025-02-19 14:59:59 +0100

    Yeah

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Gustav Jerresand',
+  description: 'Personal website of Gustav Jerresand',
   generator: 'v0.dev',
 }
 
